74154280644acbd72499284c45b05da87122409a
saspus <me@saspus.com>
2019-01-24 15:36:01 -0800

    Added persistence for machine-id

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,9 @@ mkdir -p    /config \
             /logs \
             /cache 
 
-ln -s /config/  ~/.duplicacy-web
+if [ ! -d ~/.duplicacy-web ]; then
+    ln -s /config/  ~/.duplicacy-web
+fi
  
 touch /logs/duplicacy_web.log
 
@@ -24,6 +26,16 @@ fi
 if [ ! -f /config/duplicacy.json ]; then
     echo '{}'   > /config/duplicacy.json
 
+fi
+
+if [ ! -f /etc/machine-id ]; then
+    ln -s /config/machine-id /etc/machine-id
+fi
+
+if [ ! -f /config/machine-id ]; then 
+
+    apk add dbus
+    dbus-uuidgen > /config/machine-id
 fi
 
 echo "Logging tail of the log from this moment on"
