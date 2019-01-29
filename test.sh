66d501ef403cf1a75d59a636e0eba558c008d4c8
saspus <me@saspus.com>
2019-01-29 00:57:06 -0800

    Fancyfication of test script

--- test.sh
+++ test.sh
@@ -1,12 +1,41 @@
-docker build --tag=saspus/duplicacy-web .
+#cleanup
 
-docker run 	--name duplicacy-web-container	\
-		--hostname duplicacy-web-docker \
-            	-p 3875:3875/tcp		\
-            	-v ~/Library/Duplicacy:/config 	\
-            	-v ~/Library/Logs/Duplicacy/:/logs 	\
-            	-v ~/Library/Caches/Duplicacy:/cache 	\
-            	-v /tmp/backuproot:/backuproot:ro 	\
-            	-v /tmp/storage:/storage 		\
-            	saspus/duplicacy-web 
-            
+echo Deleting container
+export container=$(docker ps -a |grep duplicacy-web-container | awk '{print $1}');
+if [ -n "$container"  ]; then 
+    docker rm $container || exit 1
+fi
+echo Deleting Image
+export image=$(docker images |grep saspus/duplicacy-web | awk '{print $3}')
+
+if  [ -n "$image" ]; then
+    docker rmi $image || exit 2
+fi
+
+temp=/tmp/dupl01
+#rm -rf $temp
+mkdir $temp
+#chmod 777 $temp
+
+
+# build
+docker build --tag=saspus/duplicacy-web .                               || exit 1
+
+
+
+#mkdir -p $temp/config $temp/logs $temp/cache $temp/storage $temp/backuproot
+#touch $temp/backuproot/yadayada $temp/backuproot/yadayada2
+#chown -R $(id -u):$(id -g) $temp/
+#run 
+docker run  --name duplicacy-web-container             \
+        --hostname duplicacy-web-docker                \
+         --publish 3875:3875/tcp                       \
+             --env USR_ID=$(id -u)                     \
+             --env GRP_ID=$(id -g)                     \
+             --env PERSISTENT_MACHINE_ID=true          \
+          --volume $temp/config:/config                \
+          --volume $temp/logs:/logs                    \
+          --volume $temp/cache:/cache                  \
+          --volume $temp/backuproot:/backuproot:ro     \
+          --volume $temp/storage:/storage              \
+                   saspus/duplicacy-web 
