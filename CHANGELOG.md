952d6b14cdc26639790231fc6b16323a6a2dac44
saspus <me@saspus.com>
2019-01-29 22:19:49 -0800

    The image now has backed link to externally stored machine-id

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 
+## [0.0.4] - 2019-01-29
+### Changed
+- Further simplified machine-id handling. The image now has backed link to externally stored machine-id
+
 ## [0.0.3] - 2019-01-29
 ### Changed
 - Optimized machine-id persistence handling.
--- init.sh
+++ init.sh
@@ -14,25 +14,21 @@ echo launch.sh runing as user $(id -un):$(id -gn)\($(id -u):$(id -g)\)
 echo "root:x:0:root" > /etc/group
 echo "root:x:0:0:root:/root:/bin/ash" > /etc/passwd
 if [ $GRP_ID -ne 0 ]; then 
-	addgroup -g $GRP_ID -S duplicacy; 
+    addgroup -g $GRP_ID -S duplicacy; 
 fi
 if [ $USR_ID -ne 0 ]; then 
-	adduser -u $USR_ID -S duplicacy -G duplicacy; 
+    adduser -u $USR_ID -S duplicacy -G duplicacy; 
 fi
  
 # Configuring folders and permissions    
-mkdir -p    				/config /logs /cache
+mkdir -p                    /config /logs /cache
 chown -R $USR_ID:$GRP_ID    /config /logs /cache
 
 # Preparing persistent unique machine ID
 if ! dbus-uuidgen --ensure=/config/machine-id; then 
-	echo machine-id contains invalid data. Regenerating.
-	dbus-uuidgen > /config/machine-id
+    echo machine-id contains invalid data. Regenerating.
+    dbus-uuidgen > /config/machine-id
 fi
-
-# Copying machine-id to container
-cp /config/machine-id /var/lib/dbus/machine-id
-chmod o+r,g+r /var/lib/dbus/machine-id
 
 echo Using machine-id = $(cat /var/lib/dbus/machine-id)
 
